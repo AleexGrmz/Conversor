c67c013dc14237a8625a5c6533294e66a93a3af3
Alejandro Gallegos <aleexgallegos1699@hotmail.com>
2023-03-02 12:56:13 -0600

    first commit

---
+++ Conversor.py
@@ -0,0 +1,15 @@
+#App para convertir pies a metros usando tkinter
+#Ramon Alejandro Gallegos Ramirez
+#23 de febrero del 2023
+
+from tkinter import* #es un paquete en python
+from tkinter import ttk
+
+class conversor:
+    #tipo constructor de la clase
+ def __init__(self, raiz):
+    raiz.title("Pies a metros")
+
+if __name__=="__main__":
+    print("Este es el archivo principal")
+    
