b0c3e6765ff43c1e7dc06bfcee21b01d529b8e98
AleexGrmz <124299427+AleexGrmz@users.noreply.github.com>
2023-03-02 14:06:10 -0600

    Update Conversor.py

--- Conversor.py
+++ Conversor.py
@@ -10,6 +10,20 @@ class conversor:
  def __init__(self, raiz):
     raiz.title("Pies a metros")
 
+    mainFrame = ttk.Frame(raiz)
+    mainFrame.grid(column=0, row=0)
+
+    piesEntry = ttk.Entry(mainFrame)
+    piesEntry.grid(column=1, row=0)
+
+    ttk.Label(mainFrame, text="Pies").grid(column=2, row=0)
+    ttk.Label(mainFrame, text="Son equivalentes a:").grid(column=0, row=1)
+    ttk.Label(mainFrame, text="Resultado").grid(column=1, row=1)
+    ttk.Label(mainFrame, text="Metros").grid(column=2, row=1)
+
+    ttk.Button(mainFrame, text="Calcular").grid(column=2, row=2)
+
 if __name__=="__main__":
     print("Este es el archivo principal")
-    
+    print("Nada mas se mostrara esto si es el principal") 
+       
